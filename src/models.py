1370bb1dc5f2a4530190e73a7b610ac47c8c48c3
KrisN9 <kristinank992@gmail.com>
2023-01-11 22:57:50 +0000

    avances en tablas de la api

--- src/models.py
+++ src/models.py
@@ -15,5 +15,69 @@ class User(db.Model):
         return {
             "id": self.id,
             "email": self.email,
-            # do not serialize the password, its a security breach
+        }
+
+class Planet(db.Model):
+    __tablename__ = 'planet'
+    id = Column(Integer, primary_key=True)
+    planet_name = Column(String(50))
+    climate = Column(String(30))
+    population = Column(Integer, nullable=True)
+
+    def __repr__(self):
+        return '<Planet %r>' % self.planet_name
+
+    def serialize(self):
+        return {
+            "id": self.id,
+            "planet": self.planet_name
+        }
+
+class People(db.Model):
+    __tablename__ = 'people'
+    id = Column(Integer, primary_key=True)
+    name = Column(String(100), nullable=False)
+    birth_date = Column(String(70), nullable=True)
+    gender = Column(String(10), nullable=False)
+    affilliation = Column(String(250), nullable=False)
+    planet_id = Column(Integer, ForeignKey('planet.id'))
+
+    def __repr__(self):
+        return '<People %r>' % self.name
+
+    def serialize(self):
+        return {
+            "id": self.id,
+            "name": self.name,
+            "affilliation": self.affilliation
+        }
+
+class FavoritePlanet(db.Model):
+    __tablename__ = 'favorite planet'
+    id = Column(Integer, primary_key=True)
+    user_id = Column(Integer, ForeignKey('user.id'))
+    planet_id = Column(Integer, ForeignKey('planet.id'))
+
+    def __repr__(self):
+        return '<FavoritePlanet %r>' % self.id
+
+    def serialize(self):
+        return {
+            "user_id": self.user_id,
+            "planet_id": self.planet_id
+        }
+
+class FavoritePeople(db.Model):
+    __tablename__ = 'favorite people'
+    id = Column(Integer, primary_key=True)
+    user_id = Column(Integer, ForeignKey('user.id'))
+    people_id = Column(Integer, ForeignKey('people.id'))
+
+    def __repr__(self):
+        return '<FavoritePeople %r>' % self.id
+
+    def serialize(self):
+        return {
+            "user_id": self.user_id,
+            "people_id": self.people_id
         }
